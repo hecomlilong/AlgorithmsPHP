68493b0b2dfd82746bdb94e786031aa93faaf875
BruceLi <lilong01@hecom.cn>
2018-02-07 22:39:27 +0800

    '2'

--- src/utils/bonusCalculator.php
+++ src/utils/bonusCalculator.php
@@ -11,8 +11,34 @@ namespace utils;
 
 class bonusCalculator
 {
-    public function getRandomMoney($remainMoney, $remainSize)
+    public static function getRandomMoney(&$remainMoney, &$remainSize)
     {
-
+		if($remainSize == 1) {
+			$remainSize--;
+			return round($remainMoney * 100) / 100;
+		}
+		
+		$r = rand(0,1);
+		$min = 0.01;
+		$max = $remainMoney / $remainSize * 2;
+		$money = $r * $max;
+		$money = $money <= $min ? 0.01 : $money;
+		$money = floor($money * 100) / 100;
+		$remainSize--;
+		$remainMoney -= $money;
+		return $money;
     }
+	
+	public static function getArray($sumMoney, $n)
+	{
+		$result = [];
+
+		while($n > 0){
+			$result[] = self::getRandomMoney($sumMoney, $n);
+		}
+		
+		return $result;
+	}
 }
+
+var_dump(bonusCalculator::getArray(100, 20));
