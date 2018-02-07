f3f74c61e03a9b991b41ab12c168e118ebf1340a
lilong <lilong@360.net>
2018-02-07 18:10:36 +0800

    ddd

---
+++ src/utils/bonusCalculator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lilong
+ * Date: 2018/2/7
+ * Time: 14:18
+ */
+
+namespace utils;
+
+
+class bonusCalculator
+{
+    public function getRandomMoney($remainMoney, $remainSize)
+    {
+
+    }
+}
